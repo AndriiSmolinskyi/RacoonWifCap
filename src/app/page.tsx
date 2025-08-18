caf935f458fbdb793dc09eb64161912964757236
AndriiiSmolinskyi <andrii.smo@cgsteam.io>
2025-08-18 17:00:06 +0300

    feat: Complete header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Page() {
       <header className="w-full h-[100px] px-7 bg-[var(--black-figma)] flex justify-between items-center sticky top-0 z-50">
         <div className="flex gap-4 items-center">
           <Image src="/avatar.svg" alt="Racoon logo" width={76} height={76} priority />
-          <h1 className="font-extrabold text-2xl">RACOONWIFCAP</h1>
+          <h1 className="font-extrabold text-2xl"> RACOON<span className="text-[var(--orange-figma)]">WIF</span>CAP</h1>
         </div>
         <nav className="flex justify-between w-1/3">
           {SECTIONS.map((id) => (
@@ -117,10 +117,10 @@ export default function Page() {
         </div>
       </header>
       <main>
-        <section id="about" className="min-h-screen p-8">ABOUT SECTION</section>
-        <section id="buy" className="min-h-screen p-8">BUY SECTION</section>
-        <section id="faq" className="min-h-screen p-8">FAQ SECTION</section>
-        <section id="roadmap" className="min-h-screen p-8">ROADMAP SECTION</section>
+        <section id="about" className="min-h-screen p-8">ABOUT RACOON SECTION</section>
+        <section id="buy" className="min-h-screen p-8">BUY RACOON SECTION</section>
+        <section id="faq" className="min-h-screen p-8">FAQ RACOON SECTION</section>
+        <section id="roadmap" className="min-h-screen p-8">ROADMAP RACOON SECTION</section>
       </main>
     </div>
   );
